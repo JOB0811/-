4e5c08fa3e7fcc19dca764aaff11c4be900b17da
JOB0811 <34418169+JOB0811@users.noreply.github.com>
2024-04-03 13:48:31 +0900

    Add files via upload

--- 경일 주원빈/24-04-02 과제/24-04-02 2번 로또 당첨기.cpp
+++ 경일 주원빈/24-04-02 과제/24-04-02 2번 로또 당첨기.cpp
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 #include<time.h>
 
-
 using std::cout;
 using std::cin;
 using std::endl;
@@ -26,62 +25,114 @@ using std::endl;
 - 5등 : 당첨번호 3개와 내가 입력한 3개의 숫자가 같을때
 */
 
-int lucky[7];  // 컴퓨터 난수 생성 저장
-int lottoNum[7]; // 사용자 지정 숫자 저장
+int lucky[6];  // 컴퓨터 난수 생성 저장
+int lottoNum[6]; // 사용자 지정 숫자 저장
 int bonusNum = 0; // 보너스 번호
-
-
+int j = 0;
+bool dupliLucky;
+bool dupliNum;
+int count = 0;
 
 
 
 void main()
 {
 	srand(time(NULL));
-		for (int i = 0; i < 7; i++)
-		{
-			lucky[i] = rand() % 45;
-			int j = i - 1;
-			while(lucky[i] == 0)
-			if (lucky[i] == 0)
-			{
-				cout << "당첨 번호중에 0이 포함되어 다시 생성하겠습니다!" << endl;
-				continue;
-			}
-			if (lucky[i] == lucky[j])
-			{
-				cout << "당첨번호중에 중복 숫자가 생성되어 다시 생성하겠습니다!" << endl;
-				continue;
-			}
-		}
-		cout << "로또 당첨을 시작하겠습니다! 사용자가 원하는 번호를 입력해주세요 :" << endl;
-		for (int i = 0; i < 7; i++)
-		{
-			cin >> lottoNum[i];
-			if (lottoNum[i] <= 0 || lottoNum[i] > 44)
-			{
-				cout << "로또 숫자의 범위는 0부터 44까지입니다! 다시 입력해주세요" << endl;
+	for (int i = 0; i < 6; i++)
+	{
+		do {
+			dupliLucky = false;
+			lucky[i] = rand() % 43 + 1;
+			bonusNum = rand() % 43 + 1;
+			for (int j = 0; i > j; j++)
+				if (lucky[i] == lucky[j])
+				{
+					cout << "당첨번호중에 중복 숫자가 생성되어 다시 생성하겠습니다!" << endl;
+					dupliLucky = true;
 					continue;
-			}
+				}
+				if (lucky[i] == bonusNum)
+				{
+					cout << "보너스 번호와 당첨번호가 중복되어 다시 생성하겠습니다!" << endl;
+					dupliLucky = true;
+					continue;
+				}
+
+			} while (dupliLucky);
 		}
 
-		bonusNum = rand() % 45;
-
-		cout << "이번 로또 당첨 추첨 번호 입니다!" << endl;
-		for (int i = 0; i < 7; i++)
-		{
-			 cout << lucky[i] << " ";
-		}
-		for (int i = 0; i < 7; i++)
-		{
-			if (lucky[0] == lottoNum[0] && lucky[1] == lottoNum[1] && lucky[2] == lottoNum[2] && lucky[3] == lottoNum[3] && lucky[4] == lottoNum[4] && lucky[5] == lottoNum[5] && lucky[6] == lottoNum[6])
-			{
-				cout << "1등에 당첨되셨습니다! 축하합니다!" << endl;
+	cout << "이번 로또 당첨 추첨 번호 입니다!" << endl;
+	for (int i = 0; i < 6; i++)
+	{
+		cout << lucky[i] << " ";
+	}
+	cout << "\n";
+	cout << "보너스 번호 : " << bonusNum << endl;
+	cout << "로또 당첨을 시작하겠습니다! 사용자가 원하는 번호를 입력해주세요 :" << endl;
+	for (int i = 0; i < 6; i++)
+	{
+		do {
+			dupliNum = false;
+			cin >> lottoNum[i];
+			for (int j = 0; i > j; j++)
+				if (lottoNum[i] == lottoNum[j])
+				{
+					cout << "중복된 숫자는 입력할 수 없습니다! 다시 입력하세요!" << endl;
+					dupliNum = true;
 					break;
+				}
+		} while (dupliNum);
+
+		if (lottoNum[i] <= 0 || lottoNum[i] > 44)
+		{
+			cout << "로또 숫자의 범위는 0부터 44까지입니다! 다시 입력해주세요" << endl;
+			continue;
+		}
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			if (lucky[i] == lottoNum[j])
+			{
+				count++;
 			}
 		}
+	}
 
+	switch (bonusNum)
+	{
+	case 1:
+		if (count == 5)
+		{
+			cout << "축하합니다! 2등입니다!";
+		}
+		break;
 
+	default:
+		if (count == 6)
+		{
+			cout << "축하합니다! 1등입니다!";
+		}
 
+		else if (count == 5)
+		{
+			cout << "축하합니다! 3등입니다!";
+		}
 
+		else if (count == 4)
+		{
+			cout << "축하합니다! 4등입니다!";
+		}
 
+		else if (count == 3)
+		{
+			cout << "축하합니다! 5등입니다!";
+		}
+		break;
+	}
 }
+
+
+
