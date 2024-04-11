5528ab52f8ae15d3eb1e96718aa31b50ef5bb146
JOB0811 <34418169+JOB0811@users.noreply.github.com>
2024-04-11 20:27:20 +0900

    Add files via upload

---
+++ 경일 주원빈/07. 문자열 + 삼항 연산자/13. 가상 함수 변환 생성자/MainGame_06.cpp
@@ -0,0 +1,16 @@
+#include "MainGame_06.h"
+#include <iostream>
+#include <vector>
+
+//↑ 생성자가 호출이 될 때, "이런 헤더파일이 존재해!"라고 알려줄 것인지, 안알려줄 것인지를 알려 주는 것
+
+
+MainGame_06::MainGame_06()
+{
+
+}
+
+MainGame_06::~MainGame_06()
+{
+
+}
